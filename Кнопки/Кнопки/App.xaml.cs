144f037b3c50f254d00fb4662a42825f93595d58
Newzorowa-Lena <Newzorowalena@gmail.com>
2025-05-19 22:42:42 +0500

    добавлены кнопки

---
+++ Кнопки/Кнопки/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace FlightSimulatorWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Инициализация глобальных ресурсов или сервисов
+            InitializeApplication();
+        }
+
+        private void InitializeApplication()
+        {
+            // Здесь можно добавить инициализацию:
+            // - Глобальных стилей
+            // - Сервисов приложения
+            // - Настроек
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // Очистка ресурсов при закрытии приложения
+            base.OnExit(e);
+        }
+    }
+}
